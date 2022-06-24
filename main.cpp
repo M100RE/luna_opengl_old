1705deaea44d03cdcc1cf77ae544981899910122
M100RE <mschnabl38@gmail.com>
2022-06-24 22:48:09 +0200

    fixxes

--- main.cpp
+++ main.cpp
@@ -7,28 +7,6 @@
 int window_width = 400;
 int window_height = 400;
 
-float vertices[]
-{
-0.979079, -1.96117, 
-1.95816, -1.96117, 
-1.95816, -0.980583, 
-0.979079, -0.980583
-};
-
-float vertices2[]
-{
-    -0.1f, -0.1f,
-    0.1f, -0.1f,
-    0.1f, 0.1f,
-    -0.1f, 0.1f
-};
-
-unsigned int indices[]
-{
-    0, 1, 2,
-    0, 3, 2
-};
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -52,20 +30,26 @@ int main()
 
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    rectangle features(rect_pos(0, 0, 50, 100, PX, PX, PX, PER), color(1, 1, 1, 1), pos_def(false, false), window_width, window_height);
-    rectangle files(rect_pos(53, 0, 170, 100, PX, PX, PX, PER), color(1, 1, 1, 1), pos_def(false, false), window_width, window_height);
-    rectangle menu(rect_pos(0, 0, 15, 70, PX, PX, PER, PER), color(1, 1, 1, 1), pos_def(false, true), window_width, window_height);
-    rectangle button(rect_pos(48, 48, 53, 53, PER, PER, PER, PER), color(1, 1, 1, 1), pos_def(false, false), window_width, window_height);
+    rectangle content(rect_pos(0, 0, 100, 100, PX, PX, PER, PER), color(32, 34, 34, 1), pos_def(false, false), window_width, window_height);
+    rectangle features(rect_pos(0, 0, 50, 100, PX, PX, PX, PER), color(62, 62, 66, 1), pos_def(true, false), window_width, window_height);
+    rectangle files(rect_pos(50, 0, 220, 100, PX, PX, PX, PER), color(45, 45, 48, 1), pos_def(false, false), window_width, window_height);
+    rectangle menu(rect_pos(0, 0, 150, 700, PX, PX, PX, PX), color(37, 37, 38, 1), pos_def(true, true), window_width, window_height);
+    rectangle button(rect_pos(48, 48, 53, 53, PER, PER, PER, PER), color(0, 122, 204, 1), pos_def(false, false), window_width, window_height);
+
+    //rectangle example(rect_pos(100, 100, 50, 50, PX, PX, PX, PX), color(255, 255, 255, 1), pos_def(false, true), window_width, window_height);
 
     while(!glfwWindowShouldClose(window))
     {
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        content.update(window_width, window_height);
+        menu.update(window_width, window_height);
         features.update(window_width, window_height);
         files.update(window_width, window_height);
-        menu.update(window_width, window_height);
         button.update(window_width, window_height);
+        
+        //example.update(window_width, window_height);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
